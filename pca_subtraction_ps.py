171f8e05161c6336eb0635cdaad3741216bb6c37
skp80286 <sameerkpatil@gmail.com>
2025-01-01 20:54:23 +0530

    Update pca_subtraction_ps.py

--- pca_subtraction_ps.py
+++ pca_subtraction_ps.py
@@ -6,8 +6,9 @@ import argparse
 import F21DataLoader as dl
 import glob
 import f21_predict_base as base
+import F21Stats as f21stats
 
-def load_dataset(datafiles, psbatchsize=100, ps_bins=None, limitsamplesize=None):
+def load_dataset(datafiles, psbatchsize=100, ps_bins=None, limitsamplesize=None, logbinning=False):
     # Create processor with desired number of worker threads
     processor = dl.F21DataLoader(max_workers=1, psbatchsize=psbatchsize, skip_ps=False, ps_bins=None, limitsamplesize=limitsamplesize, scale_ps=True)#, limitsamplesize=4)
         
@@ -34,12 +35,14 @@ def load_dataset(datafiles, psbatchsize=100, ps_bins=None, limitsamplesize=None)
     """
     #base.plot_los(all_los[:1], all_freq[0,:], showplots=True, saveplots = False, label=f"{all_params[0]}")
     #base.plot_power_spectra(all_ps[:1], all_ks[:1], all_params[:1], showplots=True, saveplots = False, label=f"{all_params[0]}")
-
+    if logbinning:
+        all_ks, all_ps = f21stats.logbin_power_spectrum_by_k(all_ks, all_ps, silent=False)
     print(f"\nCombined data shape: {all_los.shape}")
     print(f"Combined parameters shape: {all_params.shape}")
     return (all_params, all_freq, all_los, all_ps, all_ks)
 
 def bootstrap(ps):
+    if ps.shape[0] >= ps.shape[1]: return ps
     dim = ps.shape[1]
     print(f"bootstrapping. {ps.shape}")
     reps = dim//ps.shape[0]
@@ -64,20 +67,25 @@ parser.add_argument('-i', '--t_int', type=float, default=500, help='integration 
 parser.add_argument('-f', '--log_fx', type=str, default='-1.00', help='log10(f_X)')
 parser.add_argument('-x', '--xHI', type=str, default='0.25', help='mean neutral hydrogen fraction')
 parser.add_argument('-m', '--runmode', type=str, default='', help='')
+parser.add_argument('-l', '--use_log_binning', action='store_true', help='log binning of PS')
 
 args = parser.parse_args()
 output_dir = base.create_output_dir(args)
+logger = base.setup_logging(output_dir)
 #logger = base.setup_logging(output_dir)
 # File path
 so_file_path = str('%sF21_signalonly_21cmFAST_200Mpc_z%.1f_fX%s_xHI%s_%dkHz.dat' % 
                (args.path, args.redshift,args.log_fx, args.xHI, args.spec_res))
+no_file_path = str('%sF21_noiseonly_21cmFAST_200Mpc_z%.1f_%s_%dkHz_t%dh_Smin%.1fmJy_alphaR%.2f.dat' % 
+               (args.path, args.redshift, args.telescope, args.spec_res, args.t_int, args.s147, args.alpha_r))
 file_path = str('%sF21_noisy_21cmFAST_200Mpc_z%.1f_fX%s_xHI%s_%s_%dkHz_t%dh_Smin%.1fmJy_alphaR%.2f.dat' % 
                (args.path, args.redshift,args.log_fx, args.xHI, args.telescope, args.spec_res, args.t_int, args.s147, args.alpha_r))
 
 print(file_path)
 # Read the file and extract data
-params, freq, los, ps, ks = load_dataset([file_path], psbatchsize=10, limitsamplesize=1000, ps_bins=None)
-_, _, losso, psso, _ = load_dataset([so_file_path], psbatchsize=1000, limitsamplesize=1000, ps_bins=None)
+params, freq, los, ps, ks = load_dataset([file_path], psbatchsize=1, limitsamplesize=1000, ps_bins=None, logbinning=args.use_log_binning)
+_, _, losso, psso, _ = load_dataset([so_file_path], psbatchsize=1000, limitsamplesize=1000, ps_bins=None, logbinning=args.use_log_binning)
+_, _, losno, psno, _ = load_dataset([no_file_path], psbatchsize=1000, limitsamplesize=1000, ps_bins=None, logbinning=args.use_log_binning)
 ps_mean = np.mean(ps, axis=0)
 ps = bootstrap(ps)
 
@@ -85,7 +93,8 @@ print(f"Shape of ps: {ps.shape}")
 print(f"Shape of psso: {psso.shape}")
 
 base.initplt()
-plt.plot(ks[0], psso[0], label=f"Signalonly", color="green", alpha=0.4)
+plt.loglog(ks[0], psso[0], label=f"Signalonly", color="green", alpha=0.4)
+plt.loglog(ks[0], psno[0], "r--", linewidth=0.5, label="Noise")
 plt.xlabel("k (Hz^-1)")
 plt.ylabel("kP(k)")
 plt.title(f"Original Signal PS without Noise\n(z={args.redshift}, xHI_mean={args.xHI}, logfX={args.log_fx})")
@@ -110,7 +119,7 @@ for c in range(num_components):
         print(f"min_mse={min_mse}, best_component={best_component}")
 
     #print(f"Shape of transformed_data {transformed_data.shape}")
-    component = transformed_data[c].reshape((1,1382))
+    component = transformed_data[c].reshape((1,ps.shape[1]))
     #print(f"Shape of component: {component.shape}")
     #reconstructed_data = pca.inverse_transform(component)
     mse = mean_squared_error(psso, component)
@@ -130,15 +139,16 @@ for c in range(num_components):
 
 # Compare original and reconstructed sightlines
 sightline_index = 0  # Choose a sightline to compare
-noisy_ps = ps[sightline_index]
+noisy_ps = ps_mean
 ks = ks[sightline_index]
 reconstructed_ps = best_reconstruction
 print(f"min_mse={min_mse}, best_component={best_component}, best_reconstruction_type={best_reconstruction_type}")
 # Plot the original and reconstructed sightlines
 base.initplt()
-plt.plot(ks, noisy_ps, label="Original PS Sample", color="blue", alpha=0.4)
-plt.plot(ks, reconstructed_ps[0]+2e-6, label=f"Reconstructed (using mode # {best_component})", color="red", alpha=0.4)
-plt.plot(ks, psso[0]+4e-6, label=f"Signalonly", color="green", alpha=0.4)
+plt.loglog(ks, noisy_ps, label="Original PS Mean", color="blue", alpha=0.4)
+plt.loglog(ks, reconstructed_ps[0], label=f"Reconstructed (using mode # {best_component})", color="red", alpha=0.4)
+plt.loglog(ks, psso[0], label=f"Signalonly", color="green", alpha=0.4)
+plt.loglog(ks, psno[0], "r--", linewidth=0.5, label="Noise")
 plt.xlabel("k (Hz^-1)")
 plt.ylabel("kP(k)")
 plt.title(f"Comparison of Original and Best Reconstructed PS\n(z={args.redshift}, xHI_mean={args.xHI}, logfX={args.log_fx})")
