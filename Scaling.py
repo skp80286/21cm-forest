27e3ddb527dac162ce7fdc0d8eb271a157dbdbb3
skp80286 <sameerkpatil@gmail.com>
2025-01-09 14:07:42 +0530

    support none for X or y parameter

--- Scaling.py
+++ Scaling.py
@@ -9,36 +9,38 @@ class Scaler:
         self.args = args
 
     def scaleXy(self, X, y):
-        if self.args.scale_y: 
-            xHI = y[:, 0].reshape(len(y), 1)
-            scaledfx = (0.8 + y[:,1]/5.0).reshape(len(y), 1)
-            y = np.hstack((xHI, scaledfx))
-        if self.args.scale_y0: y[:,0] = y[:,0]*5.0
-        if self.args.scale_y1:
-            # we wish to create a single metric representing the expected
-            # strength of the signal based on xHI (0 to 1) and logfx (-4 to +1)
-            # We know that higher xHI and lower logfx lead to a stronger signal, 
-            # First we scale logfx to range of 0 to 1.
-            # Then we take a product of xHI and (1 - logfx)
-            if self.args.trials == 1: logger.info(f"Before scaleXy: {y}")
-            xHI = y[:, 0].reshape(len(y), 1)
-            scaledfx = 1 - (0.8 + y[:,1]/5.0)
-            product = np.sqrt(xHI**2 + scaledfx**2).reshape(len(y), 1)
-            y = np.hstack((xHI, scaledfx, product))
-            if self.args.trials == 1: logger.info(f"ScaledXy: {y}")
-        if self.args.scale_y2:
-            # we wish to create a single metric representing the expected
-            # strength of the signal based on xHI (0 to 1) and logfx (-4 to +1)
-            # We know that higher xHI and lower logfx lead to a stronger signal, 
-            # First we scale logfx to range of 0 to 1.
-            # Then we take a product of xHI and (1 - logfx)
-            if self.args.trials == 1: logger.info(f"Before scaleXy: {y}")
-            xHI = y[:, 0].reshape(len(y), 1)
-            scaledfx = 1 - (0.8 + y[:,1]/5.0).reshape(len(y), 1)
-            product = np.sqrt(xHI**2 + scaledfx**2).reshape(len(y), 1)
-            y = np.hstack((xHI, scaledfx, product))
-            if self.args.trials == 1: logger.info(f"ScaledXy: {y}")
-        if self.args.logscale_X: X = np.log(np.clip(X, 1e-20, None))
+        if y is not None:
+            if self.args.scale_y: 
+                xHI = y[:, 0].reshape(len(y), 1)
+                scaledfx = (0.8 + y[:,1]/5.0).reshape(len(y), 1)
+                y = np.hstack((xHI, scaledfx))
+            if self.args.scale_y0: y[:,0] = y[:,0]*5.0
+            if self.args.scale_y1:
+                # we wish to create a single metric representing the expected
+                # strength of the signal based on xHI (0 to 1) and logfx (-4 to +1)
+                # We know that higher xHI and lower logfx lead to a stronger signal, 
+                # First we scale logfx to range of 0 to 1.
+                # Then we take a product of xHI and (1 - logfx)
+                if self.args.trials == 1: logger.info(f"Before scaleXy: {y}")
+                xHI = y[:, 0].reshape(len(y), 1)
+                scaledfx = 1 - (0.8 + y[:,1]/5.0)
+                product = np.sqrt(xHI**2 + scaledfx**2).reshape(len(y), 1)
+                y = np.hstack((xHI, scaledfx, product))
+                if self.args.trials == 1: logger.info(f"ScaledXy: {y}")
+            if self.args.scale_y2:
+                # we wish to create a single metric representing the expected
+                # strength of the signal based on xHI (0 to 1) and logfx (-4 to +1)
+                # We know that higher xHI and lower logfx lead to a stronger signal, 
+                # First we scale logfx to range of 0 to 1.
+                # Then we take a product of xHI and (1 - logfx)
+                if self.args.trials == 1: logger.info(f"Before scaleXy: {y}")
+                xHI = y[:, 0].reshape(len(y), 1)
+                scaledfx = 1 - (0.8 + y[:,1]/5.0).reshape(len(y), 1)
+                product = np.sqrt(xHI**2 + scaledfx**2).reshape(len(y), 1)
+                y = np.hstack((xHI, scaledfx, product))
+                if self.args.trials == 1: logger.info(f"ScaledXy: {y}")
+        if X is not None:
+            if self.args.logscale_X: X = np.log(np.clip(X, 1e-20, None))
         return X, y
 
     def unscaleXy(self, X, y):
